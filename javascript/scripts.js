60a79a66b8f446ae621f554012c31daeab8ceb21
Cecilia Wang <xinranwangx@gmail.com>
2022-07-15 06:43:21 +0300

    Menu fixes

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -8,3 +8,12 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
+
+var icon=document.getElementsByClassName("fa-bars")[0];
+if(icon){
+icon.className = icon.className.replace("fa-bars", "fa-xmark");
+}
+else{
+icon=document.getElementsByClassName("fa-xmark")[0];
+icon.className = icon.className.replace("fa-xmark", "fa-bars");
+}
